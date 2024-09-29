0b7400c2300a4d4c34d948df8484f17cd9a9944a
ammarzafar4372 <ammarzafar4372@gmail.com>
2024-09-29 15:24:39 +0500

    6 step done

--- command_explore.go
+++ command_explore.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func commandExplore(cfg *Config, name string) error {
+	fmt.Printf("Exploring %s...\n", name)
 	data, err := cfg.Client.GetLocation(name)
 	if err != nil {
 		return err
 	}
+	fmt.Println("Found Pokemons:")
 	for _, v := range data.Pokemons {
-		fmt.Println(v.Pokemon.Name)
+		fmt.Printf(" - %s \n", v.Pokemon.Name)
 	}
 	return nil
 }
